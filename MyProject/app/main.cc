1eda85248ff4634dfef190a8703d6db2dc96fa20
Dave Taylor <ddataylor@gmail.com>
2023-10-27 17:53:17 +0100

    21. External Libraries: Git Submodule

--- MyProject/app/main.cc
+++ MyProject/app/main.cc
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include <nlohmann/json.hpp>
+
 #include "my_lib.h"
 #include "config.hpp"
 
@@ -7,6 +9,11 @@ int main()
 {
     std::cout << project_name << '\n';
     std::cout << project_version << '\n';
+
+    std::cout << "JSON Lin Version:"
+        << NLOHMANN_JSON_VERSION_MAJOR << "."
+        << NLOHMANN_JSON_VERSION_MINOR << "."
+        << NLOHMANN_JSON_VERSION_PATCH << "\n";
 
     print_hello_world();
 
