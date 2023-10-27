d17e9406f7e786953a5bf445639e8e9697c1f413
Dave Taylor <ddataylor@gmail.com>
2023-10-27 18:39:49 +0100

    23. External Libraries: Fetch Content

--- MyProject/app/main.cc
+++ MyProject/app/main.cc
@@ -1,6 +1,9 @@
 #include <iostream>
 
+#include <cxxopts.hpp>
 #include <nlohmann/json.hpp>
+#include <fmt/format.h>
+#include <spdlog/spdlog.h>
 
 #include "my_lib.h"
 #include "config.hpp"
@@ -10,10 +13,23 @@ int main()
     std::cout << project_name << '\n';
     std::cout << project_version << '\n';
 
-    std::cout << "JSON Lin Version:"
+    std::cout << "JSON Lib Version:"
         << NLOHMANN_JSON_VERSION_MAJOR << "."
         << NLOHMANN_JSON_VERSION_MINOR << "."
         << NLOHMANN_JSON_VERSION_PATCH << "\n";
+
+    std::cout << "FMT Version:"
+        << FMT_VERSION << "\n";
+
+    std::cout << "CXXOpts Version:"
+        << CXXOPTS__VERSION_MAJOR << "."
+        << CXXOPTS__VERSION_MINOR << "."
+        << CXXOPTS__VERSION_PATCH << "\n";
+
+    std::cout << "SPDLOG Version:"
+        << SPDLOG_VER_MAJOR << "."
+        << SPDLOG_VER_MINOR << "."
+        << SPDLOG_VER_PATCH << "\n";
 
     print_hello_world();
 
--- MyProject/src/my_lib/my_lib.cc
+++ MyProject/src/my_lib/my_lib.cc
@@ -1,6 +1,13 @@
 #include <iostream>
 
+#include <nlohmann/json.hpp>
+
 void print_hello_world()
 {
     std::cout << "Hello World\n";
+
+    std::cout << "JSON Lib Version:"
+        << NLOHMANN_JSON_VERSION_MAJOR << "."
+        << NLOHMANN_JSON_VERSION_MINOR << "."
+        << NLOHMANN_JSON_VERSION_PATCH << "\n";
 }
