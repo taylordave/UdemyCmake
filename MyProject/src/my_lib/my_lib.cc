abebcc6444e26acfce5a09898316d1f436af3310
Dave Taylor <ddataylor@gmail.com>
2023-10-28 13:20:51 +0100

    25. Unit Testing with Catch2

--- MyProject/src/my_lib/my_lib.cc
+++ MyProject/src/my_lib/my_lib.cc
@@ -1,5 +1,5 @@
+#include <cstdint>
 #include <iostream>
-
 #include <nlohmann/json.hpp>
 
 #include "my_lib.h"
@@ -16,4 +16,9 @@ void print_hello_world()
         << NLOHMANN_JSON_VERSION_MAJOR << "."
         << NLOHMANN_JSON_VERSION_MINOR << "."
         << NLOHMANN_JSON_VERSION_PATCH << "\n";
+}
+
+std::uint32_t factorial(std::uint32_t number)
+{
+    return number <= 1 ? 1 : factorial(number - 1) * number;
 }
